587f829507b2cb59e41f431270df84caa9852460
Denisa Judea <denisa.judea.jd@gmail.com>
2020-04-13 19:47:46 +0300

    parametrized tests

--- src/test/java/NituRazvan_JudeaDenisa_Lab3/AppTest.java
+++ src/test/java/NituRazvan_JudeaDenisa_Lab3/AppTest.java
@@ -2,7 +2,6 @@ package NituRazvan_JudeaDenisa_Lab3;
 
 import static org.junit.Assert.assertEquals;
 import NituRazvan_JudeaDenisa_Lab3.domain.Student;
-import NituRazvan_JudeaDenisa_Lab3.domain.Tema;
 import NituRazvan_JudeaDenisa_Lab3.repository.NotaXMLRepo;
 import NituRazvan_JudeaDenisa_Lab3.repository.StudentXMLRepo;
 import NituRazvan_JudeaDenisa_Lab3.repository.TemaXMLRepo;
@@ -10,7 +9,13 @@ import NituRazvan_JudeaDenisa_Lab3.service.Service;
 import NituRazvan_JudeaDenisa_Lab3.validation.NotaValidator;
 import NituRazvan_JudeaDenisa_Lab3.validation.StudentValidator;
 import NituRazvan_JudeaDenisa_Lab3.validation.TemaValidator;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 
 /**
@@ -20,7 +25,7 @@ import org.junit.Test;
 public class AppTest {
     StudentValidator studentValidator = new StudentValidator();
     TemaValidator temaValidator = new TemaValidator();
-    String filenameStudent;
+    String filenameStudent = "files/Studenti.xml";
     String filenameTema = "files/Teme.xml";
     String filenameNota = "files/Note.xml";
 
@@ -30,134 +35,111 @@ public class AppTest {
     NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
     Service service;
     Student student = new Student("nrie2378", "Razvan Nitu", 935, "mrie2378@scs.ubbcluj.ro");
-    Tema tema = new Tema("8931", "desciere", 5, 10);
 
-    // WHITE-BOX TESTING
-    @Test
-    public void Deadline_TC_1()
+    @BeforeEach
+    public void init()
     {
-        RefreshData();
-        tema.setDeadline(7);
-        assertEquals(TestTema(tema), true);
-        DeleteTema(tema);
-    }
-    @Test
-    public void Deadline_TC_2()
-    {
-        RefreshData();
-        tema.setDeadline(18);
-        assertEquals(TestTema(tema), false);
-        DeleteTema(tema);
-    }
-
-    // BLACK-BOX TESTING
-    @Test
-    public void Group_TC_1() {
-        RefreshData();
-        student.setGrupa(935);
-        assertEquals(Test(student), true);
-        DeleteData(student);
-    }
-
-    @Test
-    public void Group_TC_2() {
-        RefreshData();
-        student.setGrupa(99);
-        assertEquals(Test(student), false);
-        DeleteData(student);
-    }
-
-    @Test
-    public void Name_TC_6() {
-        RefreshData();
-        student.setNume("Razvan Nitu");
-        assertEquals(Test(student), true);
-        DeleteData(student);
-    }
-    @Test
-    public void Name_TC_7() {
-        RefreshData();
-        student.setNume("j");
-        assertEquals(Test(student), false);
-        DeleteData(student);
-    }
-    @Test
-    public void Name_TC_11() {
-        RefreshData();
-        student.setNume("Razvan123");
-        assertEquals(Test(student), false);
-        DeleteData(student);
-
-    }
-    @Test
-    public void Email_TC_12() {
-        RefreshData();
-        student.setEmail("denisa@scs.ro");
-        assertEquals(Test(student), true);
-        DeleteData(student);
-
-    }
-    @Test
-    public void Email_TC_15() {
-        RefreshData();
-        student.setEmail("denisa.scs.ro");
-        assertEquals(Test(student), false);
-        DeleteData(student);
-
-    }
-
-    @Test
-    public void Id_TC_16() {
-        RefreshData();
-        student.setID("jdie2338");
-        assertEquals(Test(student), true);
-        DeleteData(student);
-
-    }
-
-    @Test
-    public void Id_TC_17() {
-        RefreshData();
-        student.setID("23dfg23sdf");
-        assertEquals(Test(student), false);
-        DeleteData(student);
-
-    }
-
-
-    public void RefreshData()
-    {
-        String fisierStudenti = "files/Studenti.xml";
-        String fisierTeme = "files/Teme.xml";
-        String fisierNote = "files/Note.xml";
-        filenameTema = fisierTeme;
-        filenameNota =  fisierNote;
-        filenameStudent = fisierStudenti;
         studentXMLRepository = new StudentXMLRepo(filenameStudent);
         service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
     }
 
-    public void DeleteTema(Tema tema)
-    {
-        if (service.findTema(tema.getID()) != null)
-            service.deleteTema(tema.getID());
-    }
-
-    public void DeleteData(Student student)
+    @AfterEach
+    public void reset()
     {
         if (service.findStudent(student.getID()) != null)
             service.deleteStudent(student.getID());
     }
 
-    public Boolean Test(Student student)
+    @ParameterizedTest
+    @CsvSource({"935,true", "99,false"})
+    public void Group_TC1_TC2(int grupa, boolean expected){
+        student.setGrupa(grupa);
+        assertEquals(Test(student), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Razvan Nitu,true", "j,false","Razvan123,false"})
+    public void Name_TC6_TC7_TC11(String nume, boolean expected){
+        student.setNume(nume);
+        assertEquals(Test(student), expected);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({"denisa@scs.ro,true", "denisa.scs.ro,false"})
+    public void Email_TC12_TC15(String email, boolean expected){
+        student.setEmail(email);
+        assertEquals(Test(student), expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"jdie2338,true", "23dfg23sdf,false"})
+    public void Id_TC16_TC17(String id, boolean expected){
+        student.setID(id);
+        assertEquals(Test(student), expected);
+    }
+
+    // BLACK-BOX TESTING
+//    @Test
+//    public void Group_TC_1() {
+//        student.setGrupa(935);
+//        assertEquals(Test(student), true);
+//    }
+//
+//    @Test
+//    public void Group_TC_2() {
+//        student.setGrupa(99);
+//        assertEquals(Test(student), false);
+//    }
+
+//    @Test
+//    public void Name_TC_6() {
+//        student.setNume("Razvan Nitu");
+//        assertEquals(Test(student), true);
+//    }
+//
+//    @Test
+//    public void Name_TC_7() {
+//        student.setNume("j");
+//        assertEquals(Test(student), false);
+//    }
+
+//    @Test
+//    public void Name_TC_11() {
+//        student.setNume("Razvan123");
+//        assertEquals(Test(student), false);
+//    }
+
+//    @Test
+//    public void Email_TC_12() {
+//        student.setEmail("denisa@scs.ro");
+//        assertEquals(Test(student), true);
+//    }
+//
+//    @Test
+//    public void Email_TC_15() {
+//        student.setEmail("denisa.scs.ro");
+//        assertEquals(Test(student), false);
+//    }
+
+//    @Test
+//    public void Id_TC_16() {
+//        student.setID("jdie2338");
+//        assertEquals(Test(student), true);
+//    }
+//
+//    @Test
+//    public void Id_TC_17() {
+//        student.setID("23dfg23sdf");
+//        assertEquals(Test(student), false);
+//    }
+
+
+    private Boolean Test(Student student)
     {
         service.addStudent(student);
         return service.findStudent(student.getID()) == student;
     }
 
-    public Boolean TestTema(Tema tema)
-    {
-        service.addTema(tema);
-        return service.findTema(tema.getID()) == tema;
-    }
+
 }
---
+++ src/test/java/NituRazvan_JudeaDenisa_Lab3/WhiteBoxTests.java
@@ -0,0 +1,73 @@
+package NituRazvan_JudeaDenisa_Lab3;
+
+import static org.junit.Assert.assertEquals;
+import NituRazvan_JudeaDenisa_Lab3.domain.Tema;
+import NituRazvan_JudeaDenisa_Lab3.repository.NotaXMLRepo;
+import NituRazvan_JudeaDenisa_Lab3.repository.StudentXMLRepo;
+import NituRazvan_JudeaDenisa_Lab3.repository.TemaXMLRepo;
+import NituRazvan_JudeaDenisa_Lab3.service.Service;
+import NituRazvan_JudeaDenisa_Lab3.validation.NotaValidator;
+import NituRazvan_JudeaDenisa_Lab3.validation.StudentValidator;
+import NituRazvan_JudeaDenisa_Lab3.validation.TemaValidator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+
+/**
+ * @author Xps 9560
+ */
+public class WhiteBoxTests {
+    StudentValidator studentValidator = new StudentValidator();
+    TemaValidator temaValidator = new TemaValidator();
+    String filenameStudent = "files/Studenti.xml";
+    String filenameTema = "files/Teme.xml";
+    String filenameNota = "files/Note.xml";
+
+
+    StudentXMLRepo studentXMLRepository;
+    TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+    NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+    NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+    Service service;
+    Tema tema = new Tema("8931", "descriere", 5, 10);
+
+    @Before
+    public void init(){
+        studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    @After
+    public void reset()
+    {
+        if (service.findTema(tema.getID()) != null)
+            service.deleteTema(tema.getID());
+    }
+
+
+    // WHITE-BOX TESTING - FOR LAB3 In Class!!!!
+    @Test
+    public void Deadline_TC_1()
+    {
+        tema.setDeadline(7);
+        assertEquals(TestTema(tema), true);
+    }
+
+    @Test
+    public void Deadline_TC_2()
+    {
+        tema.setDeadline(18);
+        assertEquals(TestTema(tema), false);
+    }
+
+
+    private Boolean TestTema(Tema tema)
+    {
+        service.addTema(tema);
+        return service.findTema(tema.getID()) == tema;
+    }
+
+}
